1a0f02c308bd531037d221a85ab8a408cdd51fb2
cmartinezal <cmartinez@teleworx.com>
2024-04-11 17:43:54 -0500

    Add user endpoints

---
+++ src/routes/v1/users/__init__.py
@@ -0,0 +1,50 @@
+
+"""
+Endpoints for Users
+"""
+import json
+from flask import Blueprint, jsonify, request
+from ...utils.database import get_db_data_by_value
+from .helpers import *
+
+
+blueprint = Blueprint('users', __name__, url_prefix='/api/v1')
+
+
+@blueprint.route('/users', methods=['GET', 'POST'])
+def users() -> dict:
+    """Get a list of users or create new"""
+
+    if request.method == "GET":
+        return get_users()
+
+    if request.method == "POST":
+        body = request.get_json()
+        return post_user(body)
+
+
+@blueprint.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def user(id: int) -> dict:
+    """Get, update or delete existing User"""
+
+    query = """
+    SELECT U.id, U.first_name, U.last_name, U.email, U.created_date FROM USERS U WHERE U.id = ?;
+    """
+
+    user_list = get_db_data_by_value(query, [id])
+
+    if user_list is None or len(user_list) == 0:
+        return jsonify({'error': 'User not found.'}), 404
+
+    if 'error' in user_list:
+        return user_list, 500
+
+    if request.method == "GET":
+        return jsonify(user_list[0])
+
+    if request.method == "PUT":
+        body = json.loads(request.data)
+        return put_user(body, query, id)
+
+    if request.method == "DELETE":
+        return delete_user(id)
---
+++ src/routes/v1/users/helpers.py
@@ -0,0 +1,81 @@
+"""
+Helper functions for Users
+"""
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import jsonify
+from typing import List
+from ...utils.validations import validate_user_body
+from ...utils.database import get_db_data, get_db_data_by_value, insert_row, update_row, delete_row
+
+
+def get_users() -> dict:
+    """Get all Users"""
+
+    query = """
+    SELECT U.id, U.first_name, U.last_name, U.email, U.created_date FROM USERS U;
+    """
+    user_list = get_db_data(query)
+
+    if user_list is None or len(user_list) == 0:
+        return jsonify({'error': 'users not found.'}), 404
+
+    if 'error' in user_list:
+        return user_list, 500
+
+    return jsonify(user_list)
+
+
+def post_user(body: dict) -> dict:
+    """Create a new User"""
+
+    select_query = """
+    SELECT U.id, U.first_name, U.last_name, U.email, U.created_date FROM USERS U WHERE U.id = (SELECT MAX(id) FROM USERS);
+    """
+    if not body or not validate_user_body(body):
+        return jsonify({'error': 'Invalid User properties.'}), 400
+
+    first_name = body.get("first_name", "")
+    last_name = body.get("last_name", "")
+    email = body.get("email", "")
+    password = generate_password_hash(body.get("password", ""))
+    response = insert_row(
+        'INSERT INTO USERS(first_name, last_name, email, password) VALUES (?, ?, ?, ?);', [first_name, last_name, email, password], select_query)
+
+    if response and 'error' in response:
+        return response, 500
+
+    return jsonify(response[0]), 201
+
+
+def put_user(body: dict, query: str, id: int) -> dict:
+    """Update a User"""
+
+    if not validate_user_body(body):
+        return jsonify({'error': 'Invalid User properties.'}), 400
+
+    first_name = body.get("first_name", "")
+    last_name = body.get("last_name", "")
+    email = body.get("email", "")
+    password = generate_password_hash(body.get("password", ""))
+
+    update_query = """
+    UPDATE USERS SET
+    first_name = ?, last_name = ?, email = ?, password = ?
+    WHERE id = ?;
+    """
+    response = update_row(
+        update_query, [first_name, last_name, email, password, id], id, query)
+
+    if response and 'error' in response:
+        return response, 500
+
+    return jsonify(response[0])
+
+
+def delete_user(id: int) -> dict:
+    """Delete a User"""
+
+    delete_row(
+        'DELETE FROM USERS WHERE id = ?;', [id])
+    return '', 204
